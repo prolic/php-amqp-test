5b60700ea009303c7046a7fa412887e5c043442a
prolic <saschaprolic@googlemail.com>
2016-07-14 15:28:25 +0800

    initial commit

---
+++ tests/PersistentConnectionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare (strict_types=1);
+
+namespace Prolic\PhpAmqpTest;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * Class PersistentConnectionTest
+ * @package Prolic\PhpAmqpTest
+ */
+class PersistentConnectionTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_uses_persistent_connection()
+    {
+        $connection = new \AMQPConnection();
+        $connection->pconnect();
+
+        $connection = new \AMQPConnection();
+        $connection->pconnect();
+    }
+}
